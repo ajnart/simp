3f64224150a3828ce83749837e4bafa9dc98e0ff
Aj - Thomas <thomas.camlong@epitech.eu>
2021-10-27 16:12:34 +0200

    Add placeholder for localStorage

--- cli/main.ts
+++ cli/main.ts
@@ -1,6 +1,15 @@
+import { customAlgorithm } from "./models/algorithm.ts";
+
 // TODO: Implement cli interface
-// computeResult(
-//   new customAlgorithm("{domainName.firstLetter+1}{domainLenght*2}{master}"),
-//   "Toto",
-//   "gmail.com",
-// );
+const algorithm = new customAlgorithm(
+  "{domainName.firstLetter+1}gen{domainLenght*2}{master}",
+);
+const parsedTokens = algorithm.getParsedTokens();
+let masterPassword: string | null = localStorage.getItem("masterPassword");
+if (masterPassword) {
+  console.log(`Found master password: ${masterPassword}`);
+} else {
+  console.log(`No master password found`);
+  masterPassword = prompt("Master password:");
+  localStorage.setItem("masterPassword", masterPassword as string);
+}
