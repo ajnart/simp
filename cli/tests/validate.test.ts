59ebfa0d479813d164bb0440b0b076c99e3cf23a
Aj - Thomas <thomas.camlong@epitech.eu>
2022-01-25 12:24:07 +0100

    Update test suite

---
+++ cli/tests/validate.test.ts
@@ -0,0 +1,16 @@
+import {
+  assertEquals,
+  assertThrows,
+} from "https://deno.land/std/testing/asserts.ts";
+
+import { validateAlgorithm } from "../models/validator.ts";
+
+Deno.test("Validate basic algorithm", () => {
+  const algorithm = "{domainName.firstLetter+1}{domainLenght*2}{master}";
+  assertEquals(validateAlgorithm(algorithm), true);
+});
+
+Deno.test("Failed to validate algorithm", () => {
+  const algorithm = "{domainName.firstLetter+1}{domainLenght*2}";
+  assertThrows(() => validateAlgorithm(algorithm));
+});
