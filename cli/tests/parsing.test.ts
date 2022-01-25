2ff943cb66c32f519f2d5c1d9656705d6b6b3969
Aj - Thomas <thomas.camlong@epitech.eu>
2022-01-25 09:05:07 +0100

    Update parsing tests

--- cli/tests/parsing.test.ts
+++ cli/tests/parsing.test.ts
@@ -1,39 +1,22 @@
-import {
-  assertEquals,
-  assertThrows,
-} from "https://deno.land/std@0.110.0/testing/asserts.ts";
-import { CustomAlgorithm } from "../models/algorithm.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+
+import { parseTokens } from "../models/parser.ts";
 import { Token } from "../models/token.ts";
 
 Deno.test("A little bit of parsing", () => {
-  const algorithm = new CustomAlgorithm(
-    "{domainName.firstLetter+1}{domainLenght*2}{master}",
-  );
+  const algorithm = "{domainName.firstLetter+1}{domainLenght*2}{master}";
   const token: Token[] = [];
   token.push(new Token("domainName.firstLetter+1"));
   token.push(new Token("domainLenght*2"));
   token.push(new Token("master"));
-  assertEquals(algorithm.getParsedTokens(), token);
+  assertEquals(parseTokens(algorithm), token);
 });
 
 Deno.test("Advanced parsing", () => {
-  const algorithm = new CustomAlgorithm(
-    "abc{domainLenght*2+1}{master}",
-  );
+  const algorithm = "abc{domainLenght*2+1}{master}";
   const token: Token[] = [];
   token.push(new Token("abc"));
   token.push(new Token("domainLenght*2+1"));
   token.push(new Token("master"));
-  assertEquals(algorithm.getParsedTokens(), token);
-});
-
-Deno.test("Error: No master password", () => {
-  const algorithm = new CustomAlgorithm(
-    "{domainLenght*2+1}",
-  );
-  assertThrows(
-    algorithm.getParsedTokens.bind(algorithm),
-    Error,
-    "No master logic block in algorithm.",
-  );
+  assertEquals(parseTokens(algorithm), token);
 });
