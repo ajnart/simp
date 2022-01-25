0f63f3422ffe3add01fef9b82b947714ef56661e
Aj - Thomas <thomas.camlong@epitech.eu>
2022-01-25 12:24:45 +0100

    Update validator so it throws on exceptions

--- cli/models/validator.ts
+++ cli/models/validator.ts
@@ -1,18 +1,21 @@
 // https://www.regextester.com/94502
+
+import { ValidationError } from "https://deno.land/x/cliffy/command/mod.ts";
+
 // Turbo regex to check that a url is valid
 const URL_MATCH_REGEX =
   /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
 
-export function validateWebsite(input: string): boolean | string {
-  if (input.match(URL_MATCH_REGEX)) {
-    return true;
+export function validateWebsite(input: string): boolean {
+  if (input.match(URL_MATCH_REGEX) == null) {
+    throw new ValidationError("Invalid URL.");
   }
-  return "Invalid URL";
+  return true;
 }
 
-export function validateAlgorithm(algorithm: string): string | boolean {
+export function validateAlgorithm(algorithm: string): boolean {
   if (algorithm.search("{master}") == -1) {
-    return ("No master logic block in algorithm.");
+    throw new ValidationError("No master logic block in algorithm.");
   }
   return true;
 }
